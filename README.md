416116c1ce482d070c1acf17b85acab45a76dfe3
Casper Christiansen <casper@Caspers-MacBook-Air.local>
2020-06-19 13:47:49 +0200

    footer done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-***Mediesuset-1try by Casper***
+# mediesuset-1try by casper
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
@@ -68,4 +68,5 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
 # mediesuset-1try
--- src/components/Partials/Footer/Footer.jsx
+++ src/components/Partials/Footer/Footer.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import Fblogo from '../../../images/fblogo.png';
+import Tlogo from '../../../images/tlogo.png';
+import Hlogo from '../../../images/Hancock-kvalitet-logo.png';
 
 export default function Footer(props) {
     return (
         <footer>
             <h2>FØLG OS</h2>
+            <div className="smlogos">
+                <img src={Fblogo} alt="fblogo" className="smlogo" />
+                <img src={Tlogo} alt="tlogo" className="smlogo" />
+            </div>
+            <h2>TILMELD NYHEDSBREV</h2>
+            <p>Få seneste nyt sendt direkte til din indbakke</p>
+            <form action="" className="footerform">
+                <input type="text" className="indtast" placeholder="INDTAST DIN E-MAIL"/>
+                <button className="tilmeld">TILMELD NYHEDSBREV</button>
+            </form>
+            <img src={Hlogo} alt="hlogo"/>
+            <h2 className="fontcursive">-smag forskellen...</h2>
         </footer>
     )
 }
